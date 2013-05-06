7427f416ad03c73639b66533f63173a0c9eab11f
MrWhite76 <MrSnowy76@gmail.com>
2013-05-06 16:58:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , consolidate = require('consolidate')
+  , defaultConfig = require('./lib/src/saveDefaultConfig');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use('/public', express.static(__dirname + "/public"));
+  app.engine('html',consolidate.mustache)
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+defaultConfig.saveDefaultConfig();
+
+
+
